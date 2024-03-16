4e735ec5c4c11eaf18c9cce0fce9d4a76c7b8cf5
Gaurav <gedamgaurav121@gmail.com>
2024-03-16 21:51:09 +0530

    login and register backend

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,21 +1,50 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { json, useNavigate, useLocation  } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
+import axios from 'axios';
 import "../Styles/AuthStyles.css"
 
 
 const Login = () => {
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/api/v1/auth/login", {
+        
+        email,
+        password
+       
+      });
+      if (res && res.data.success) {
+        toast.success(res.data && res.data.message);
+
+        navigate( "/");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
   
   
   return (
      <>
       <div className="form-container ">
-        <form >
+        <form onSubmit={handleSubmit}>
           <h4 className="title">LOGIN FORM</h4>
           
           <div className="mb-3">
             <input
               type="email"
               
-              
+              value={email}
+              onChange={(e)=>{setEmail(e.target.value)}}
               className="form-control"
               id="exampleInputEmail1"
               placeholder="Enter Your Email "
@@ -26,7 +55,8 @@ const Login = () => {
             <input
               type="password"
               
-              
+              value={password}
+              onChange={(e)=>{setPassword(e.target.password)}}
               className="form-control"
               id="exampleInputPassword1"
               placeholder="Enter Your Password"
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
+import axios from 'axios';
 
 import "../Styles/AuthStyles.css"
 const Register = () => {
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [phone,setPhone] = useState("");
+  const [address,setAddress] = useState("");
+  const [answer,setAnswer] = useState("");
+  const navigate = useNavigate();
+  // form function
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/api/v1/auth/register", {
+        name,
+        email,
+        password,
+        phone,
+        address,
+        answer,
+      });
+      if (res && res.data.success) {
+        toast.success(res.data && res.data.message);
+        navigate("/login");
+      } else {
+        toast.error(res.data.message);
+        
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
 
     // form function
 
@@ -12,13 +47,14 @@ const Register = () => {
   return (
       <>
       <div className="form-container ">
-        <form >
+        <form onSubmit={handleSubmit}>
           <h4 className="title">REGISTER FORM</h4>
           <div className="mb-3">
             <input
               type="text"
               
-              
+              value={name}
+              onChange={(e)=>{setName(  e.target.value)}}
               className="form-control"
               id="exampleInputEmail1"
               placeholder="Enter Your Name"
@@ -29,7 +65,8 @@ const Register = () => {
           <div className="mb-3">
             <input
               type="email"
-              
+              value={email}
+              onChange={(e)=>{setEmail(e.target.value)}}
               className="form-control"
               id="exampleInputEmail1"
               placeholder="Enter Your Email "
@@ -39,7 +76,8 @@ const Register = () => {
           <div className="mb-3">
             <input
               type="password"
-              
+              value={password}
+              onChange={(e)=>{setPassword(e.target.value)}}
               className="form-control"
               id="exampleInputPassword1"
               placeholder="Enter Your Password"
@@ -48,7 +86,9 @@ const Register = () => {
           </div>
           <div className="mb-3">
             <input
-              type="text"
+              type="text" 
+              value={phone}
+              onChange={(e)=>{setPhone(e.target.value)}}
               
               className="form-control"
               id="exampleInputEmail1"
@@ -59,6 +99,8 @@ const Register = () => {
           <div className="mb-3">
             <input
               type="text"
+              value={address}
+              onChange={(e)=>{setAddress(e.target.value)}}
               
               className="form-control"
               id="exampleInputEmail1"
@@ -69,6 +111,8 @@ const Register = () => {
           <div className="mb-3">
             <input
               type="text"
+              value={answer}
+              onChange={(e)=>{setAnswer(e.target.value)}}
               
               className="form-control"
               id="exampleInputEmail1"
